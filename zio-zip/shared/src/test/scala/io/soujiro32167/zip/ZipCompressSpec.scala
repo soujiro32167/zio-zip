4ed634cfcf45c6ccac140e9eb5eb6a332986e682
Eli Kasik <soujiro32167@gmail.com>
2020-05-17 11:07:55 -0400

    unzip tested

--- zio-zip/shared/src/test/scala/io/soujiro32167/zip/ZipCompressSpec.scala
+++ zio-zip/shared/src/test/scala/io/soujiro32167/zip/ZipCompressSpec.scala
@@ -1,10 +1,11 @@
 package io.soujiro32167.zip
 
+import java.io.File
 import java.nio.file.Files
 import java.util.zip.ZipFile
 
 import zio.{console, _}
-import zio.stream.{Stream, ZSink}
+import zio.stream.{Stream, ZSink, ZStream}
 import zio.test.Assertion._
 import zio.test._
 
@@ -45,6 +46,18 @@ object ZipCompressSpec extends DefaultRunnableSpec{
             "myfile1" -> "f1",
             "myfile2" -> "f2"
           ))))
+    },
+
+    testM("unzip from premade file"){
+      val fruits = getClass.getResource("/fruits.zip")
+      for {
+        expected <- Task(new ZipFile(new File(fruits.toURI)))
+        .map(zip => zip -> zip.entries().asScala.toList)
+        .map{ case (zip, entries) => entries.map(entry => entry.getName -> new String(zip.getInputStream(entry).readAllBytes())) }
+        actual <- ZipCompress.unzip()(ZStream.fromInputStream(fruits.openStream()).flattenChunks)
+          .mapM{ case (name, content) => content.runCollect.map(chunks => name -> new String(chunks.flatMap(_.toList).toArray)) }
+          .runCollect
+      } yield assert(actual)(equalTo(expected))
     }
   )
 }
