4e0afde0edf0168843cfcdef281e16c9b0818a85
Eli Kasik <soujiro32167@gmail.com>
2022-10-02 15:44:29 -0400

    Cross-compile the zio2 module (#2)

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ val scala213 = "2.13.9"
 val scala3   = "3.2.0"
 
 ThisBuild / organization := "io.soujiro32167"
-ThisBuild / scalaVersion := scala3
+ThisBuild / scalaVersion := scala213
 ThisBuild / description  := "A ZIP compression lib for ZIO"
 ThisBuild / licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0"))
 ThisBuild / developers := List(
@@ -16,7 +16,20 @@ ThisBuild / resolvers += "jitpack" at "https://jitpack.io"
 addCommandAlias("fmt", "all scalafmtSbt scalafmt test:scalafmt")
 addCommandAlias("check", "all scalafmtSbtCheck scalafmtCheck test:scalafmtCheck")
 
+def commonSettings = List(
+  scalacOptions ++= {
+    CrossVersion.partialVersion(scalaVersion.value) match {
+      case Some((2, _)) => List("-Xsource:3")
+      case Some((3, _)) => List("-Ycheck-all-patmat", "-explain")
+      case _            => Nil
+    }
+  },
+  scalacOptions -= "-Xlint:infer-any", // zio.Schedule compositions infer some types as Any
+  testFrameworks := Seq(new TestFramework("zio.test.sbt.ZTestFramework"))
+)
+
 lazy val `zio1-zip-core` = (project in file("zio1-zip-core"))
+  .settings(commonSettings)
   .settings(
     name := "zio1-zip",
     libraryDependencies ++= Seq(
@@ -25,17 +38,11 @@ lazy val `zio1-zip-core` = (project in file("zio1-zip-core"))
       `dev.zio`.zio1.`test-sbt`,
       `dev.zio`.zio1.`zio-streams`
     ),
-    testFrameworks     := Seq(new TestFramework("zio.test.sbt.ZTestFramework")),
-    crossScalaVersions := Seq(scala213, scala3),
-    scalacOptions ++= {
-      CrossVersion.partialVersion(scalaVersion.value) match {
-        case Some((2, _)) => List("-Xsource:3")
-        case _            => Nil
-      }
-    }
+    crossScalaVersions := Seq(scala213, scala3)
   )
 
 lazy val `zio-zip-core` = (project in file("zio-zip-core"))
+  .settings(commonSettings)
   .settings(
     name := "zio-zip",
     libraryDependencies ++= Seq(
@@ -44,14 +51,7 @@ lazy val `zio-zip-core` = (project in file("zio-zip-core"))
       `dev.zio`.zio.`test-sbt`,
       `dev.zio`.zio.`zio-streams`
     ),
-    testFrameworks     := Seq(new TestFramework("zio.test.sbt.ZTestFramework")),
-    crossScalaVersions := Seq(scala3),
-    scalacOptions ++= {
-      CrossVersion.partialVersion(scalaVersion.value) match {
-        case Some((2, _)) => List("-Xsource:3")
-        case _            => Nil
-      }
-    }
+    crossScalaVersions := Seq(scala213, scala3)
   )
 
 lazy val `zio-zip` = (project in file("."))
--- zio-zip-core/src/test/scala/io/soujiro32167/ZipCompressSpec.scala
+++ zio-zip-core/src/test/scala/io/soujiro32167/ZipCompressSpec.scala
@@ -1,41 +1,49 @@
 package io.soujiro32167
 
 import io.soujiro32167.models.ZipEntry
+import zio.*
+import zio.stream.{ZPipeline, ZSink, ZStream}
+import zio.test.*
+import zio.test.Assertion.*
 
 import java.io.File
 import java.nio.file.Files
 import java.util.zip.ZipFile
-import zio.*
-import zio.stream.{Stream, ZPipeline, ZSink, ZStream}
-import zio.test.Assertion.*
-import zio.test.*
-
 import scala.jdk.CollectionConverters.*
 
 object ZipCompressSpec extends ZIOSpecDefault {
+  implicit class Silly[E, A](val self: ZIO[Nothing, E, A]) extends AnyVal {
+    def widen: ZIO[Any, E, A] = self.asInstanceOf[ZIO[Any, E, A]]
+  }
   def spec = suite("zip compression")(
     test("single entry") {
-      val zipFile =
+      val file =
         Files.createTempFile("derp", ".zip")
 
       val s = ZStream("myfile" -> ZStream.fromIterable("I am a file".getBytes))
-      val compressed = s
-        .viaFunction(ZipCompress.zip())
-        .run(ZSink.fromOutputStream(Files.newOutputStream(zipFile)))
-      compressed *>
-        ZIO
-          .attempt(new ZipFile(zipFile.toFile))
-          .map(zip => zip -> zip.entries().asScala.toList)
-          .map { case (zip, entries) =>
-            entries.map(entry => entry.getName -> new String(zip.getInputStream(entry).readAllBytes()))
-          }
-          .map(res => assertTrue(res == List("myfile" -> "I am a file")))
+
+      val a = for {
+        _ <- s
+          .viaFunction(ZipCompress.zip())
+          .run(ZSink.fromOutputStream(Files.newOutputStream(file)))
+          .widen
+      } yield {
+        val zipFile = new ZipFile(file.toFile)
+        val actual = zipFile
+          .entries()
+          .asScala
+          .map(entry => entry.getName -> new String(zipFile.getInputStream(entry).readAllBytes()))
+          .toList
+        assertTrue(actual == List("myfile" -> "I am a file"))
+      }
+      a
     },
     test("multi entry") {
       val zipFile = Files.createTempFile("derp", ".zip")
       ZStream("myfile1" -> ZStream.fromIterable("f1".getBytes), "myfile2" -> ZStream.fromIterable("f2".getBytes))
         .viaFunction(ZipCompress.zip())
-        .run(ZSink.fromOutputStream(Files.newOutputStream(zipFile))) *>
+        .run(ZSink.fromOutputStream(Files.newOutputStream(zipFile)))
+        .widen *>
         ZIO
           .attempt(new ZipFile(zipFile.toFile))
           .map(zip => zip -> zip.entries().asScala.toList)
@@ -53,9 +61,10 @@ object ZipCompressSpec extends ZIOSpecDefault {
           .map { case (zip, entries) =>
             entries.map(entry => entry.getName -> new String(zip.getInputStream(entry).readAllBytes()))
           }
+          .widen
         s = ZStream
           .fromInputStream(fruits.openStream())
-          .viaFunction(ZipCompress.unzip())
+          .viaFunction(ZipCompress.unzip[Any]())
         actual <- s.mapZIO { case (name, content) =>
           content
             .via(ZPipeline.utf8Decode)
@@ -68,19 +77,12 @@ object ZipCompressSpec extends ZIOSpecDefault {
       trait R1
       trait R2
       trait E1
-      trait E2
+      trait E2 extends E1
       def toCompress: ZStream[R1, E1, ZipEntry[R2, E2]] = ???
-      lazy val s: ZStream[R1 & R2, E1 | E2 | Throwable, Byte] =
+      lazy val f: ZStream[R1 & R2, E1, Byte] =
         toCompress.viaFunction(ZipCompress.zip())
 
-      extension [R, E, A](self: ZStream[R, E | Throwable, A])
-        inline def orDiePartial(implicit trace: Trace): ZStream[R, E, A] =
-          self.catchAll {
-            case e: Throwable => ZStream.die(e)
-            case e: E         => ZStream.fail(e)
-          }
-
-      lazy val s2: ZStream[R1 & R2, E1 | E2, Byte] = s.orDiePartial
+      implicitly[f.type <:< ZStream[R1 & R2, E1, Byte]]
 
       assertCompletes
     }
